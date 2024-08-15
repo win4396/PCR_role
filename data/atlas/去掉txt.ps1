9e702e1a443c4133969cd140064d256ef2f1ff37
win4396 <1078662439@qq.com>
2024-08-15 22:06:08 +0800

    New character updates

--- data/atlas/去掉txt.ps1
+++
@@ -1,6 +0,0 @@
-$txtFiles = Get-ChildItem -Filter *.txt
-foreach($file in $txtFiles){
-    $newName = $file.BaseName  
-    Rename-Item -Path $file.FullName -NewName "$newName"  
-    Write-Host "Renamed $($file.FullName) to $newName"  
-}  
